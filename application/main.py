94f1ed29fb40bc3b41e60895447838c2728f2267
Герман <german@MacBook-Air-German.local>
2021-06-03 00:29:57 +0300

    date added

--- application/main.py
+++ application/main.py
@@ -1,8 +1,8 @@
 from people import get_employees 
 from salary import calculate_salary
-import datetime
+from datetime import datetime, date
 def date():
-    print(datetime.date(year=2021, month=6, day=5))
+    print(datetime.today())
 if __name__ == '__main__':
     calculate_salary()
     get_employees()
